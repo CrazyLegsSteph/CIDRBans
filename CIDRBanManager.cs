f53508ae0702f6a8e1e466aee5ae73e1acac56d9
Thanadol Chomphoochan <aquablitz11@gmail.com>
2015-06-29 23:00:31 +0700

    Initial Commit

---
+++ CIDRBanManager.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.IO;
+
+using TShockAPI;
+using TShockAPI.DB;
+using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+
+namespace CIDRbans
+{
+    /// <summary>CIDR Ban Manager</summary>
+    public class CIDRBanManager
+    {
+        /// <summary> The IDbConnection object </summary>
+        private IDbConnection db;
+        /// <summary>Constructor for CIDRBanManager class</summary>
+        public CIDRBanManager()
+        {
+            //Initialize connection to either SQLite or MySQL database
+            switch (TShock.Config.StorageType.ToLower())
+            {
+                case "mysql":
+                    string[] host = TShock.Config.MySqlHost.Split(':');
+                    db = new MySqlConnection()
+                    {
+                        ConnectionString = String.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                        host[0],
+                        host.Length == 1 ? "3306" : host[1],
+                        TShock.Config.MySqlDbName,
+                        TShock.Config.MySqlUsername,
+                        TShock.Config.MySqlPassword)
+                    };
+                    break;
+                case "sqlite":
+                    string dbPath = Path.Combine(TShock.SavePath, "CIDRBans.sqlite");
+                    db = new SqliteConnection(String.Format("uri=file://{0},Version=3", dbPath));
+                    break;
+            }
+            SqlTableCreator creator = new SqlTableCreator(db, db.GetSqlType() == SqlType.Sqlite ? 
+                (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+                creator.EnsureTableStructure(new SqlTable("CIDRBans",
+                    new SqlColumn("CIDR", MySqlDbType.String) { Primary = true },
+                    new SqlColumn("Reason", MySqlDbType.Text),
+                    new SqlColumn("BanningUser", MySqlDbType.Text),
+                    new SqlColumn("Date", MySqlDbType.Text),
+                    new SqlColumn("Expiration", MySqlDbType.Text)));
+        }
+
+        /// <summary>Search CIDR bans with given IP</summary>
+        /// <param name="IP">IP string for searching</param>
+        /// <returns>CIDRBan object</returns>
+        public CIDRBan GetCIDRBanByIP(string check)
+        {
+            List<CIDRBan> banlist = new List<CIDRBan>();
+            try
+            {
+                using (var reader = db.QueryReader("SELECT * FROM CIDRBans"))
+                {
+                    while (reader.Read())
+                        banlist.Add(new CIDRBan(reader.Get<string>("CIDR"), reader.Get<string>("Reason"),
+                            reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration")));
+                    //Check if the IP matches any of range in CIDR Ban List
+                    foreach (CIDRBan ban in banlist)
+                    {
+                        if (CIDRBan.Check(check, ban.CIDR))
+                            return ban;
+                        //If not, continue to next CIDR ban in range
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return null;
+        }
+
+        /// <summary>Search for all CIDR ranges banned</summary>
+        /// <returns>List of CIDRBan objects</returns>
+        public List<CIDRBan> GetCIDRBanList()
+        {
+            List<CIDRBan> banlist = new List<CIDRBan>();
+            try
+            {
+                using (var reader = db.QueryReader("SELECT * FROM CIDRBans"))
+                {
+                    while (reader.Read())
+                        banlist.Add(new CIDRBan(reader.Get<string>("CIDR"), reader.Get<string>("Reason"),
+                            reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration")));
+                    return banlist;
+                }
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return new List<CIDRBan>();
+        }
+
+        /// <summary>Add a CIDR range to ban list</summary>
+        /// <param name="cidr">CIDR Range</param>
+        /// <param name="reason">Ban Reason</param>
+        /// <param name="user">Banning User</param>
+        /// <param name="date">Date of banning</param>
+        /// <param name="expire">Expiration Period</param>
+        /// <returns>Success</returns>
+        public bool AddCIDRBan(string cidr, string reason = "", string user = "", string date = "", string expire = "")
+        {
+            try
+            {
+                return db.Query("INSERT INTO CIDRBans (CIDR, Reason, BanningUser, Date, Expiration) " +
+                    "VALUES (@0, @1, @2, @3, @4)", cidr, reason, user, date, expire) != 0;
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return false;
+        }
+
+        /// <summary>Delete specified CIDR range</summary>
+        /// <param name="cidr">CIDR Range</param>
+        /// <returns>Success</returns>
+        public bool DelCIDRBanByRange(string cidr)
+        {
+            try
+            {
+                return db.Query("DELETE FROM CIDRBans WHERE CIDR = @0", cidr) != 0;
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return false;
+        }
+
+        /// <summary>Delete all CIDR ranges matched up with an IP</summary>
+        /// <param name="ip">Specified IP</param>
+        /// <returns>List of CIDR range strings</returns>
+        public List<string> DelCIDRBanByIP(string ip)
+        {
+            List<CIDRBan> banlist = new List<CIDRBan>();
+            List<string> removelist = new List<string>();
+            try
+            {
+                using (var reader = db.QueryReader("SELECT * FROM CIDRBans"))
+                {
+                    while (reader.Read())
+                        banlist.Add(new CIDRBan(reader.Get<string>("CIDR"), reader.Get<string>("Reason"),
+                            reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration")));
+                    //Check if the IP matches any of range in CIDR Ban List
+                    foreach (CIDRBan ban in banlist)
+                    {
+                        if (CIDRBan.Check(ip, ban.CIDR))
+                            removelist.Add(ban.CIDR);
+                        //If true, add it to list for removal preparation
+                        //Loop until every CIDR range in list is checked
+                    }
+                }
+                //Start removing everything in prepared list
+                foreach (string remove in removelist)
+                    db.Query("DELETE FROM CIDRBans WHERE CIDR = @0", remove);
+                //Inform players
+                return removelist;
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return new List<string>();
+        }
+    }
+
+    /// <summary>CIDR Ban Information</summary>
+    public class CIDRBan
+    {
+        /// <summary>CIDR Range</summary>
+        public string CIDR { get; set; }
+        /// <summary>Ban Reason</summary>
+        public string Reason { get; set; }
+        /// <summary>Banning User</summary>
+        public string BanningUser { get; set; }
+        /// <summary>Date of banning</summary>
+        public string Date { get; set; }
+        /// <summary>Date of ban expiration</summary>
+        public string Expiration { get; set; }
+
+        /// <summary>Constructor for creating CIDRBan class</summary>
+        /// <param name="cidr">CIDR Range</param>
+        /// <param name="reason">Ban Reason</param>
+        /// <param name="user">Banning User</param>
+        /// <param name="date">Date of banning</param>
+        /// <param name="expire">Expiration Period</param>
+        public CIDRBan(string cidr, string reason, string user, string date, string expire)
+        {
+            this.CIDR = cidr;
+            this.Reason = reason;
+            this.BanningUser = user;
+            this.Date = date;
+            this.Expiration = expire;
+        }
+
+        /// <summary>Blank constructor for CIDRBan class</summary>
+        public CIDRBan()
+        {
+            this.CIDR = "";
+            this.Reason = "";
+            this.BanningUser = "";
+            this.Date = "";
+            this.Expiration = "";
+        }
+
+        /// <summary>
+        /// Check if IP is included in CIDR Range or not
+        /// </summary>
+        /// <param name="check">IP to check</param>
+        /// <param name="cidr">CIDR Range</param>
+        /// <returns></returns>
+        public static bool Check(string check, string cidr)
+        {
+            const uint defaultmask = 0xffffffff;
+            int maskcount = Convert.ToInt32(cidr.Split('/')[1]);
+            //mask = 32-bit mask for using & operator
+            uint mask = Convert.ToUInt32(defaultmask << (32 - maskcount));
+            //cidrbyteparts = List with 4 byte objects containing each part of CIDR IP
+            List<byte> cidrbyteparts = (from num in cidr.Split('/')[0].Split('.')
+                                        select Convert.ToByte(num)).ToList();
+            //cidrip = Translate CIDR IP to a number ranging from 0 to 2^32
+            uint cidrip = (((uint)cidrbyteparts[0] * (uint)Math.Pow(2, 24)) + ((uint)cidrbyteparts[1] * (uint)Math.Pow(2, 16)) +
+                ((uint)cidrbyteparts[2] * (uint)Math.Pow(2, 8)) + (uint)cidrbyteparts[3]);
+            //Cut the part which is not meant to be the same out, for checking
+            cidrip &= mask;
+            //checkbyteparts = List with 4 byte objects containing each part of check IP
+            List<byte> checkbyteparts = (from num in check.Split('.')
+                                         select Convert.ToByte(num)).ToList();
+            //checkip = Translate check IP to a number ranging from 0 to 2^32
+            uint checkip = (((uint)checkbyteparts[0] * (uint)Math.Pow(2, 24)) + ((uint)checkbyteparts[1] * (uint)Math.Pow(2, 16)) +
+                ((uint)checkbyteparts[2] * (uint)Math.Pow(2, 8)) + (uint)checkbyteparts[3]);
+            //Cut the part which is not meant to be the same out, for checking
+            checkip &= mask;
+            //Check the front part which is meant to be the same if it is same or not
+            if (cidrip == checkip)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ CIDRBansPlugin.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace CIDRbans
+{
+    [ApiVersion(1, 17)]
+    public class CIDRBansPlugin : TerrariaPlugin
+    {
+        /// <summary>Plugin Name string</summary>
+        public override string Name { get { return "CIDR Bans"; } }
+        /// <summary>Plugin Description string</summary>
+        public override string Description { get { return "Allows banning CIDR ranges"; } }
+        /// <summary>Plugin Author string</summary>
+        public override string Author { get { return "AquaBlitz11"; } }
+        /// <summary>Plugin Version object</summary>
+        public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+        /// <summary>Plugin Constructor</summary>
+        public CIDRBansPlugin(Main game) : base(game) { }
+
+        /// <summary>CIDRBanManager object</summary>
+        private CIDRBanManager cidrbans;
+        /// <summary>CIDR Range Regular Expression string</summary>
+        private const string rangeregex = @"^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}\/(3[0-2]|[1-2]?[0-9])$";
+        /// <summary>IP Regular Expression string</summary>
+        private const string ipregex = @"^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$";
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+        }
+
+        protected override void Dispose(bool Disposing)
+        {
+            if (Disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+                ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+            }
+            base.Dispose(Disposing);
+        }
+
+        /// <summary>Fired when server initalizes Terraria</summary>
+        /// <param name="args">The EventArgs object</param>
+        public void OnInitialize(EventArgs args)
+        {
+            //Initialize database
+            cidrbans = new CIDRBanManager();
+            //Adds CIDRBan command
+            Commands.ChatCommands.Add(new Command("cidrbans.use", CIDRBanCommand, "cidrban")
+            {
+                HelpDesc = new[]
+                {
+                    "{0}cidrban add <range> [reason] - Ban a CIDR range permanently.".SFormat(Commands.Specifier),
+                    "{0}cidrban addtemp <range> <time> [reason] - Ban a CIDR range temporarily.".SFormat(Commands.Specifier),
+                    "{0}cidrban del <ip/range> - Unban CIDR range or ranges that includes specified IP.".SFormat(Commands.Specifier),
+                    "{0}cidrban list - List all CIDR ranges banned in the system.".SFormat(Commands.Specifier)
+                }
+            });
+        }
+
+        /// <summary>Fired when a player joins the server</summary>
+        /// <param name="args">The JoinEventArgs object</param>
+        public void OnJoin(JoinEventArgs args)
+        {
+            //If player was disconnected by another component, skip
+            if (args.Handled)
+                return;
+            //If IP banning isn't used, skip
+            if (!TShock.Config.EnableIPBans)
+                return;
+            //Creates TSPlayer player object for easy access
+            TSPlayer player = TShock.Players[args.Who];
+            //Searches a ban by player's IP
+            CIDRBan ban = cidrbans.GetCIDRBanByIP(player.IP);
+            //If no ban is found or ban has expired, skip
+            if (ban == null)
+                return;
+            //DateTime exp object
+            DateTime exp;
+            //If player's ban has no expiration date, say ban forever
+            if (!DateTime.TryParse(ban.Expiration, out exp))
+                player.Disconnect("You are banned forever: " + ban.Reason);
+            //If player's ban is temporary, check if it has expired or not
+            else
+            {
+                //If player's ban has expired, remove the ban
+                if (DateTime.UtcNow >= exp)
+                {
+                    cidrbans.DelCIDRBanByRange(ban.CIDR);
+                    return;
+                }
+                //Finds time left from now to expiration date
+                TimeSpan ts = exp - DateTime.UtcNow;
+                //Makes 30 days count shows as one month
+                int months = ts.Days / 30;
+                //Converts timespan object into time string to inform player
+                if (months > 0)
+                {
+                    player.Disconnect(String.Format("You are banned for {0} month{1} and {2} day{3}: {4}",
+                        months, months == 1 ? "" : "s", ts.Days, ts.Days == 1 ? "" : "s", ban.Reason));
+                }
+                else if (ts.Days > 0)
+                {
+                    player.Disconnect(String.Format("You are banned for {0} day{1} and {2} hour{3}: {4}",
+                        ts.Days, ts.Days == 1 ? "" : "s", ts.Hours, ts.Hours == 1 ? "" : "s", ban.Reason));
+                }
+                else if (ts.Hours > 0)
+                {
+                    player.Disconnect(String.Format("You are banned for {0} hour{1} and {2} minute{3}: {4}",
+                        ts.Hours, ts.Hours == 1 ? "" : "s", ts.Minutes, ts.Minutes == 1 ? "" : "s", ban.Reason));
+                }
+                else if (ts.Minutes > 0)
+                {
+                    player.Disconnect(String.Format("You are banned for {0} minute{1} and {2} second{3}: {4}",
+                        ts.Minutes, ts.Minutes == 1 ? "" : "s", ts.Seconds, ts.Seconds == 1 ? "" : "s", ban.Reason));
+                }
+                else
+                {
+                    player.Disconnect(String.Format("You are banned for {0} second{1}: {2}",
+                        ts.Seconds, ts.Seconds == 1 ? "" : "s", ban.Reason));
+                }
+            }
+            //Sets player's connection handled
+            args.Handled = true;
+        }
+
+        /// <summary>Fired when CIDR Ban command is used</summary>
+        /// <param name="args">CommandArgs object</param>
+        private void CIDRBanCommand(CommandArgs args)
+        {
+            //Creates TSPlayer player object for easy access
+            TSPlayer player = args.Player;
+            //Creates subcmd as shortcut for sub-command used (args.Parameters[0])
+            string subcmd = args.Parameters.Count == 0 ? "help" : args.Parameters[0].ToLower();
+            //Checks which sub-command is used
+            switch (subcmd)
+            {
+                case "help":
+                    //Displays CIDR Bans Plugin's information
+                    player.SendInfoMessage("CIDR Bans Plugin");
+                    player.SendInfoMessage("Description: Allows banning CIDR ranges");
+                    player.SendInfoMessage("Syntax: {0}cidrban <add/addtemp/del/list> [arguments]", Commands.Specifier);
+                    player.SendInfoMessage("Type {0}help cidrban for more info.", Commands.Specifier);
+                    break;
+                case "add":
+                    //Checks if player has put in CIDR range
+                    if (args.Parameters.Count < 2)
+                    {
+                        player.SendErrorMessage("Invalid syntax! Proper syntax: {0}cidrban add <range> [reason]", Commands.Specifier);
+                        return;
+                    }
+                    //Checks if player's CIDR range input is correct
+                    if (!Regex.IsMatch(args.Parameters[1], rangeregex))
+                    {
+                        player.SendErrorMessage("Invalid CIDR range string! Proper format: 0-255.0-255.0-255.0-255/0-32");
+                        return;
+                    }
+                    //If player hasn't specified reason, set default reason
+                    if (args.Parameters.Count < 3)
+                        args.Parameters.Add("Manually added IP address ban.");
+                    //For reason with spaces without use of quotation marks, put into one string
+                    args.Parameters[2] = String.Join(" ", args.Parameters.GetRange(2, args.Parameters.Count - 2));
+                    //Add CIDR range into database
+                    if (cidrbans.AddCIDRBan(args.Parameters[1], args.Parameters[2], player.Name, DateTime.UtcNow.ToString("s")))
+                        player.SendSuccessMessage("Banned range {0} for '{1}'.", args.Parameters[1], args.Parameters[2]);
+                    //If it fails, inform player
+                    else
+                        player.SendErrorMessage("Adding range {0} into database failed.", args.Parameters[1]);
+                    break;
+                case "addtemp":
+                    //Checks if player has put in CIDR range and time
+                    if (args.Parameters.Count < 3)
+                    {
+                        player.SendErrorMessage("Invalid syntax! Proper syntax: {0}cidrban addtemp <range> <time> [reason]", Commands.Specifier);
+                        return;
+                    }
+                    //Checks if player's CIDR range input is correct
+                    if (!Regex.IsMatch(args.Parameters[1], rangeregex))
+                    {
+                        player.SendErrorMessage("Invalid CIDR range string! Proper format: 0-255.0-255.0-255.0-255/0-32");
+                        return;
+                    }
+                    //Creates int exp object to store expiration time in seconds
+                    int exp;
+                    //Parse input argument into exp object
+                    if (!TShock.Utils.TryParseTime(args.Parameters[2], out exp))
+                    {
+                        //If fails, inform player
+                        args.Player.SendErrorMessage("Invalid time string! Proper format: _d_h_m_s, with at least one time specifier.");
+                        args.Player.SendErrorMessage("For example, 1d and 10h-30m+2m are both valid time strings, but 2 is not.");
+                        return;
+                    }
+                    //If player hasn't specified reason, set default reason
+                    if (args.Parameters.Count < 4)
+                        args.Parameters.Add("Manually added IP address ban.");
+                    //For reason with spaces without use of quotation marks, put into one string
+                    args.Parameters[3] = String.Join(" ", args.Parameters.GetRange(3, args.Parameters.Count - 3));
+                    //Add CIDR range into database
+                    if (cidrbans.AddCIDRBan(args.Parameters[1], args.Parameters[3], player.Name,
+                        DateTime.UtcNow.ToString("s"), DateTime.UtcNow.AddSeconds(exp).ToString("s")))
+                        player.SendSuccessMessage("Banned range {0} for '{1}'.", args.Parameters[1], args.Parameters[3]);
+                    //If it fails, inform player
+                    else
+                        player.SendErrorMessage("Adding range {0} into database failed.", args.Parameters[1]);
+                    break;
+                case "del":
+                    //Checks if player has put in either CIDR range or IP
+                    if (args.Parameters.Count < 2)
+                    {
+                        player.SendErrorMessage("Invalid syntax! Proper syntax: {0}cidrban del <ip/range>", Commands.Specifier);
+                        return;
+                    }
+                    //Checks if input is CIDR range
+                    if (Regex.IsMatch(args.Parameters[1], rangeregex))
+                    {
+                        //Removes ban from database
+                        if (cidrbans.DelCIDRBanByRange(args.Parameters[1]))
+                            player.SendSuccessMessage("Unbanned range {0}.", args.Parameters[1]);
+                        //If it fails, inform player
+                        else
+                            player.SendErrorMessage("Removing range {0} from database failed.", args.Parameters[1]);
+                        return;
+                    }
+                    //Checks if input is IP
+                    if (Regex.IsMatch(args.Parameters[1], ipregex))
+                    {
+                        //List of string for removed CIDR ranges
+                        List<string> removed = cidrbans.DelCIDRBanByIP(args.Parameters[1]);
+                        //If no CIDR ranges are removed, inform player that it fails
+                        if (removed.Count == 0)
+                        {
+                            player.SendErrorMessage("Removing range {0} from database failed.", args.Parameters[1]);
+                            return;
+                        }
+                        //Inform player amount of ranges removed and which
+                        player.SendSuccessMessage("Removed {0} range{1} from the database:", removed.Count, removed.Count == 1 ? "" : "s");
+                        player.SendInfoMessage(String.Join(", ", removed));
+                        return;
+                    }
+                    //If player's input doesn't match either range or IP, inform player
+                    player.SendErrorMessage("Invalid syntax! Proper syntax: {0}cidrban del <ip/range>", Commands.Specifier);
+                    player.SendErrorMessage("IP proper format: 0-255.0-255.0-255.0-255");
+                    player.SendErrorMessage("CIDR range proper format : 0-255.0-255.0-255.0-255/0-32");
+                    break;
+                case "list":
+                    //PageNumber integer to use with TShock's built-in PaginationTools
+                    int pagenumber;
+                    if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, args.Player, out pagenumber))
+                        return;
+                    //List of all CIDR Ban Informations
+                    List<CIDRBan> list = cidrbans.GetCIDRBanList();
+                    //Creates list of CIDR ranges
+                    var namelist = from ban in list
+                                            select ban.CIDR;
+                    //Inform player, allows selecting page
+                    PaginationTools.SendPage(player, pagenumber, PaginationTools.BuildLinesFromTerms(namelist),
+                            new PaginationTools.Settings
+                            {
+                                HeaderFormat = "CIDR Range Bans ({0}/{1}):",
+                                FooterFormat = "Type {0}ban list {{0}} for more.".SFormat(Commands.Specifier),
+                                NothingToDisplayString = "There are currently no CIDR range bans."
+                            });
+                    break;
+                default:
+                    player.SendErrorMessage("Invalid subcommand. Type {0}help cidrban for information.", Commands.Specifier);
+                    break;
+            }
+        }
+    }
+}
